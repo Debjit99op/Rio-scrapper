75aa89b2099b9f463378aa87e883cf0a9d5c474a
Debjit99op <laand6486@gmail.com>
2025-02-07 01:00:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,11 @@
-# Copyright (C) @Debjit71
-# Channel: https://t.me/Rio_scrapper
-
 # Pyrogram setup
 API_ID = "26330824"  # Replace this API ID with your actual API ID
 API_HASH = "b555d42aaf84f5266a201abfbf5ef53f"  # Replace this API HASH with your actual API HASH
-SESSION_STRING = "BQGRxsgAdNdGKSS40mhd7zKacUzIFKB3UtJ1QV8bdevqkStIfbcr2ehxIrrqgpPKibNbyO20r2AF-vZexciZfr7z4OWnmVe96bAlc1P6dsRkxpqN7cHxz_gntq4_4e0o-o5o47xE8EfaqgP_dXk2OREt7h0Pg2oki_s6IeV9IJoCt17tGvkIPlOEyzLUt3-5_jFFDdQ8sZLJeBxmeck2ixazkDVlch50Cq9h8cnkpbQSqePzDyoFHyWfvpYTfD7p0tdXdZOF2yVfYs5t6esW50wVKWBBV7-PpmGHT6itynpwjawWsuH2Edwcft8s2TO_KSNl-xRdy0z1olD5JIcsm4cOHO71YAAAAAGYYWSZAA"  # Replace this BOT_TOKEN
+SESSION_STRING = "BQGRxsgAdNdGKSS40mhd7zKacUzIFKB3UtJ1QV8bdevqkStIfbcr2ehxIrrqgpPKibNbyO20r2AF-vZexciZfr7z4OWnmVe96bAlc1P6dsRkxpqN7cHxz_gntq4_4e0o-o5o47xE8EfaqgP_dXk2OREt7h0Pg2oki_s6IeV9IJoCt17tGvkIPlOEyzLUt3-5_jFFDdQ8sZLJeBxmeck2ixazkDVlch50Cq9h8cnkpbQSqePzDyoFHyWfvpYTfD7p0tdXdZOF2yVfYs5t6esW50wVKWBBV7-PpmGHT6itynpwjawWsuH2Edwcft8s2TO_KSNl-xRdy0z1olD5JIcsm4cOHO71YAAAAAGYYWSZAA"  # Replace this SESSION STRING with your actual SESSION_STRING
+BOT_TOKEN = "7618455538:AAFDdUJ3xt-qgcyv6aGMYqiNcniZjxvCwfk"  # Replace this BOT_TOKEN
 
 # Admin IDs
-ADMIN_IDS = [6952940943, 6725302937, 7660546867]
+ADMIN_IDS = [7660546867, 8071612110]
 
 # Limits
 DEFAULT_LIMIT = 10000 # Card Scrapping Limit For Everyone
